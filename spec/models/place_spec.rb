1e947faa3ca107482d0994ae0e3d6cec3f722a38
AlexIv <alx.ekb@gmail.com>
2019-02-22 15:35:01 +0500

    first test passed

--- spec/models/place_spec.rb
+++ spec/models/place_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Place, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before(:each) do
+    @place = Place.create!(title: 'Place Title', description: 'Place description here')
+  end
+
+  describe "creation" do
+    it "should have one item created after being created" do
+      expect(Place.all.count).to eq(1)
+    end
+  end
 end
+
+# t.string "title"
+# t.text "description"
+# t.datetime "created_at", null: false
+# t.datetime "updated_at", null: false
